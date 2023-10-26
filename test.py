e3491cfc103f032144939fa748f4f9fd797a0bae
Maxim Semkin <semkin.maksim@mail.ru>
2023-10-26 20:56:01 +0300

    handling a failed test

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
 class TestResult:
     def __init__(self):
         self.runCount = 0
+        self.errorCount = 0
+
+    def testFailed(self):
+        self.errorCount = self.errorCount + 1
 
     def testStarted(self):
         self.runCount = self.runCount + 1
 
     def summary(self):
-        return "%d run, 0 failed" % self.runCount
+        return "%d run, %d failed" % (self.runCount, self.errorCount)
 
 class TestCase:
     def __init__(self, name):
@@ -19,7 +23,11 @@ class TestCase:
         result = TestResult()
         result.testStarted()
         self.setUp()
-        exec("self." + self.name + "()")
+        try:
+            method = getattr(self, self.name)
+            method()
+        except:
+            result.testFailed()
         self.tearDown()
         return result
 
@@ -53,8 +61,15 @@ class TestCaseTest(TestCase):
     def testFailedResult(self):
         test = WasRun("testBrokenMethod")
         result = test.run()
-        assert("1 run, 1 failed", result.summary)
+        assert("1 run, 1 failed" == result.summary())
 
-TestCaseTest("testTemplateMethod").run()
-TestCaseTest("testResult").run()
-TestCaseTest("testFailedResult").run()
+    def testFailedResultFormatting(self):
+        result = TestResult()
+        result.testStarted()
+        result.testFailed()
+        assert("1 run, 1 failed" == result.summary())
+
+print(TestCaseTest("testTemplateMethod").run().summary())
+print(TestCaseTest("testResult").run().summary())
+print(TestCaseTest("testFailedResult").run().summary())
+print(TestCaseTest("testFailedResultFormatting").run().summary())
